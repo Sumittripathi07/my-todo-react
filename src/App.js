b64cd91df0734c8bf445adbf1903e761273664bc
Sumit Tripathi <sumit.tpt07@gmail.com>
2023-04-19 01:17:11 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -25,7 +25,23 @@ function App() {
       });
       setTodos(todosArray);
     });
+    return () => unsub();
   }, []);
+
+  const handleEdit = async (todo, title) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+
   return (
     <>
       <div className="App">
@@ -34,6 +50,17 @@ function App() {
         </div>
         <div>
           <AddTodo />
+        </div>
+        <div className="todo_container">
+          {todos.map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            />
+          ))}
         </div>
       </div>
     </>
